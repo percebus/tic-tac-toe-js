0e8b1dac104d672774a79681b469446172751d89
JC Guerrero <JCGuerrero@MicroSoft.com>
2022-06-09 10:14:33 -0500

    feat(app)
    tic-tac-toe example form ReactJS.com

---
+++ src/Components/Square/Square.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+class Square extends React.Component {
+  render() {
+    return (
+      <button className="square">
+        {/* TODO */}
+      </button>
+    );
+  }
+
+  // TODO
+//   render_i(i) {
+//     return <Square />;
+//   }
+}
+
+export default Square
